37c60638ee8f806749e71e9bcbfe1dcb4641155c
Krish47007 <krishnendu.gupta00@gmail.com>
2026-06-02 22:46:09 +0530

    Basic greetings endpoint added

---
+++ src/test/java/com/krish/jenkins_ci_cd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,26 @@
+package com.krish.jenkins_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class JenkinsCiCdApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void testGreetingEndpoint() throws Exception {
+		String name = "Krish";
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}",name))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.string("Hello "+name +", Congrats!!!! You have successfully configured Jenkins CI/CD"));
+	}
+
+}
